336a9b4eab2810f7c6aa4ddd2327fe283052d9b3
Mo Chahadeh <applednd2002@gmail.com>
2022-07-01 16:27:28 +0300

    added launch function with mongoose

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -10,4 +11,27 @@ app.get("/", (req,res) => {
 
 })
 
-app.listen(3000, () => console.log("listening on port 6930"));
+
+async function launch() {
+
+    if(!process.env.MONGODB_URI) return console.error("No Mongodb url provided");
+
+    console.log("Connecting...");
+    try {
+
+        const connection = await mongoose.connect(process.env.MONGODB_URI, {socketTimeoutMS: 1000});
+
+        console.log("Connected to MongoDB");
+    } catch(err) {
+
+        console.error("Could not connected to Database...");
+        console.log(err);
+        return process.kill(1);
+
+    }
+
+    return app.listen(process.env.PORT || 3001, ()=> console.log("Listening on port " + process.env.PORT));
+
+}
+
+launch();
