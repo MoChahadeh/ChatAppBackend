d1fd977e91fe54c40a3a02012f62ad13999aefed
Mo Chahadeh <applednd2002@gmail.com>
2022-07-03 00:14:38 +0300

    added fetch route

---
+++ routes/fetch.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import authMidWare from '../middleware/auth.js';
+import {Conversation} from '../db.js';
+
+
+const router = express.Router();
+
+router.get("/", authMidWare, async (req, res) => {
+
+    const convos = await Conversation.find({users: req.user._id}).populate("users");
+
+    if(!convos) return res.status(400).send("No conversations found");
+
+    return res.status(200).send(convos);
+
+})
+
+
+export default router;
