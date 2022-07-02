8d43892c6a516170c5eda5fd8e9342b361257724
Mo Chahadeh <applednd2002@gmail.com>
2022-07-03 00:15:39 +0300

    added send route

---
+++ routes/send.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import { User, Conversation } from '../db.js';
+import authMidWare from '../middleware/auth.js';
+
+const router = express.Router();
+
+
+router.post("/", authMidWare, async (req, res) => {
+
+    const from = await User.findById(req.user._id);
+
+    if(!from) return res.status(400).send("invalid sender");
+
+    const to = await User.findOne({email: req.body.email});
+
+    if(!to) return res.status(404).send("Contact not found");
+
+    let convo = Conversation.findOne({users: [from._id, to._id]});
+
+    if(!convo) {
+
+        convo = new Conversation({users: [from._id, to._id], messages: [{sender: from._id, message: req.body.message}]});
+
+        try {
+
+            await convo.save();
+    
+            return res.status(200).send()
+    
+        } catch(err) {
+
+            return res.status(400).send(err.message);
+
+        }
+    } else {
+
+        try{
+
+            convo.messages.push({sender: from._id, message: req.body.message});
+
+            await convo.save();
+
+            return res.status(200).send(convo);
+
+        } catch(err) {
+
+            return res.status(400).send(err.message);
+
+        }
+
+    }
+
+    
+
+})
+
+export default router;
