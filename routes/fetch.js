0ccbcf7b400a1c53dbc1a00456ac0a00c3818869
Mo Chahadeh <applednd2002@gmail.com>
2022-07-07 19:41:43 +0400

    added selection to user populating

--- routes/fetch.js
+++ routes/fetch.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get("/", authMidWare, async (req, res) => {
 
-    const convos = await Conversation.find({users: req.user._id}).populate("users");
+    const convos = await Conversation.find({users: req.user._id}).populate("users", ["_id", "name", "email"]);
 
     if(!convos) return res.status(400).send("No conversations found");
 
